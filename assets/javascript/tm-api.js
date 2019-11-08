ecb4b1aeed31d1fdc5991a55c6687ee368fb0293
wparks08 <wparks08@gmail.com>
2019-11-07 19:47:21 -0800

    add target variable to search fns

--- assets/javascript/tm-api.js
+++ assets/javascript/tm-api.js
@@ -6,7 +6,7 @@ const EVENT_SEARCH = "events.json?";
  * Search using the parameters defined in @options
  * @param {Object} options 
  */
-function search(options) {
+function search(options, target) {
     let queryUrl = ROOT_URL + EVENT_SEARCH + "apikey=" + API_KEY;
 
     options.classificationName = "music";
@@ -24,7 +24,7 @@ function search(options) {
     }).then(function (result) {
         // return result
         console.log(result);
-        displayResult(result);
+        displayResult(result, target);
     }, function (error) {
         console.log(error);
     });
@@ -49,27 +49,27 @@ function getEventById(id) {
  * Search for music event by a keyword
  * @param {string} keyword 
  */
-function searchByKeyword(keyword) {
+function searchByKeyword(keyword, target) {
     search({
         keyword: keyword
-    });
+    }, target);
 }
 
 /**
  * Search for a music event by city
  * @param {string} city 
  */
-function searchByCity(city) {
+function searchByCity(city, target) {
     search({
         city: city
-    })
+    }, target)
 }
 
-function searchByKeywordAndCity(keyword, city) {
+function searchByKeywordAndCity(keyword, city, target) {
     search({
         keyword: keyword,
         city: city
-    });
+    }, target);
 }
 
 //onclick function to push data information into upcoming events cards
@@ -79,13 +79,13 @@ $("#hack-it").on("click", function (event) {
     // $(".card--content").empty();
     $("#card-row-container").empty();
     var valueArtist = $("#artistSearch").val().trim();
-    searchByKeyword(valueArtist);
+    searchByKeyword(valueArtist, "#card-row-container");
 })
 
 
 
-function displayResult(result) {
-    let target = $("#card-row-container");
+function displayResult(result, target) {
+    let targetElement = $(target);
     result["_embedded"].events.forEach(function (event) {
         var properties = getEventProperties(event);
 
@@ -137,7 +137,7 @@ function displayResult(result) {
             )
         );
 
-        target.append(container);
+        targetElement.append(container);
 
 
 
